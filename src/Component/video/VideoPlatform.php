71d4b15ca28190b8a4232e86d6a8a43416368daa
manu7772 <manu7772@gmail.com>
2025-11-05 18:19:17 +0100

    Try load HTML file in DOMDocument 2

--- src/Component/video/VideoPlatform.php
+++ src/Component/video/VideoPlatform.php
@@ -80,10 +80,13 @@ abstract class VideoPlatform implements VideoPlatformInterface
     {
         if(!isset($this->source_code) || $force) {
             if($url = $this->getGeneratedUrl()) {
+                libxml_use_internal_errors(true);
                 $this->source_code = new DOMDocument();
                 if(!@$this->source_code->loadHTMLFile($url, LIBXML_NOERROR | LIBXML_NOWARNING)) {
                     $this->source_code = null;
                 }
+                libxml_clear_errors();
+                libxml_use_internal_errors(false);
             } else {
                 $this->source_code = null;
             }
