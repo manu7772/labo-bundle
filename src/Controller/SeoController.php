d0abeb3d822617678129694159256d158225b211
manu7772 <manu7772@gmail.com>
2026-09-12 06:58:25 +0200

    SeoController

--- src/Controller/SeoController.php
+++ src/Controller/SeoController.php
@@ -8,13 +8,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SeoController extends AbstractController
 {
 
-    #[Route('/{robots}.txt', name: 'robots_txt', methods: ['GET'])]
-    protected function robotsTxt(): Response
+    /**
+     * Get content of xml file if exists
+     * 
+     * @param string $file
+     * @return Response
+     */
+    #[Route('/{file}.xml', name: 'xml_file', methods: ['GET'], priority: -1)]
+    protected function xmlFile(
+        string $file,
+    ): Response
+    {
+        $response = new Response(status: Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'application/xml');
+        if($xml = file_get_contents(sprintf('%s.%s', $file, 'xml'))) {
+            $response->setContent($xml);
+        } else {
+            // Generate 404
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            $response->setContent(null);
+        }
+        return $response;
+    }
+
+
+    /**
+     * Get content of txt file if exists
+     * 
+     * @param string $file
+     * @return Response
+     */
+    #[Route('/{file}.txt', name: 'txt_file', methods: ['GET'], priority: -1)]
+    protected function fileTxt(
+        string $file,
+    ): Response
     {
         $response = new Response(status: Response::HTTP_OK);
         $response->headers->set('Content-Type', 'text/plain');
-        $txt = file_get_contents('robots.txt');
-        $response->setContent($txt);
+        if($txt = file_get_contents(sprintf('%s.%s', $file, 'txt'))) {
+            $response->setContent($txt);
+        } else {
+            // Generate 404
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            $response->setContent(null);
+        }
         return $response;
     }
 
